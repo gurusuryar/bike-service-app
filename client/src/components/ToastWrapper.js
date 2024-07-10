f671c7282e777d2e3afdc024156200fe33c89112
gurusuryar <gurusuryaprakashr@gmail.com>
2024-07-10 19:36:32 +0530

    feat: Added Toastify

---
+++ client/src/components/ToastWrapper.js
@@ -0,0 +1,20 @@
+// ToastContainerWrapper.js
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const ToastWrapper = () => (
+  <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+  />
+);
+
+export default ToastWrapper;
