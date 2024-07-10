7e82bdac012da5fa2efbf61d8efca4567535e0e8
gurusuryar <gurusuryaprakashr@gmail.com>
2024-07-10 19:37:45 +0530

    feat: Updated NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import { Link } from 'react-router-dom';
-import { useTheme } from '@mui/material/styles';
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { Link, useNavigate } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (route) => {
+    handleMenuClose();
+    navigate(route);
+  };
 
   return (
     <AppBar
       position="fixed"
-      style={{ backgroundColor: '#141a24', padding: '0.4rem' }}
-      elevation={4} // Add this line to set the elevation
+      style={{
+        backgroundColor: "rgba(31, 38, 52, 0.7)", // semi-transparent background
+        backdropFilter: "blur(10px)", // blur effect
+        padding: "0.4rem",
+      }}
+      elevation={4}
     >
       <Toolbar>
         <Typography
-          variant="h2"
+          variant="h3"
           component={Link}
           to="/"
           style={{
-            textDecoration: 'none',
+            textDecoration: "none",
             color: theme.palette.text.primary,
           }}
         >
@@ -27,28 +53,45 @@ const Navbar = () => {
         </Typography>
         <div style={{ flexGrow: 1 }} />
         <Button
+        variant="inherit"
           component={Link}
           to="/register"
           style={{
             color: theme.palette.secondary.main,
-            fontSize: '1.1rem', // Set the font size here
+            fontSize: "1.1rem",
           }}
         >
-          Register
+          Sign Up
         </Button>
-        <Button
-          component={Link}
-          to="/login"
-          style={{
-            color: theme.palette.secondary.main,
-            fontSize: '1.1rem', // Set the font size here
-          }}
+        <div style={{ position: "relative", marginLeft: "1rem" }}>
+          <AccountCircle
+            onClick={handleMenuOpen}
+            style={{ color: theme.palette.secondary.main, cursor: "pointer", fontSize:"2.5rem"}}
+          />
+        </div>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          style={{ backgroundColor: theme.palette.primary.main }}
         >
-          Login
-        </Button>
-        <IconButton color="inherit" style={{ color: theme.palette.text.primary }}>
-          <AccountCircle />
-        </IconButton>
+          <MenuItem
+            component={Link}
+            to="/login"
+            onClick={() => handleMenuItemClick("/login")}
+            style={{ color: theme.palette.text.primary }}
+          >
+            Sign in
+          </MenuItem>
+          <MenuItem
+            component={Link}
+            to="/logout"
+            onClick={() => handleMenuItemClick("/logout")}
+            style={{ color: theme.palette.text.primary }}
+          >
+            Sign out
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
