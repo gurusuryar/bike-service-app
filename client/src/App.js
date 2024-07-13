15a73f1699bff061f9707e52fc9a6a95dec416a7
gurusuryar <gurusuryaprakashr@gmail.com>
2024-07-13 05:38:47 +0530

    chore: update project configurations and main application file
    - Updated index.html for better structure
    - Modified manifest.json with new settings
    - Updated App.js for new features and improvements

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,34 @@ import ToastContainerWrapper from './components/ToastWrapper';
 import Home from './pages/Home';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
-import OwnerDash from './pages/OwnerDash';
-import CustomerDash from './pages/CustomerDash';
+import OwnerServicePage from './pages/OwnerServicePage';
+import CustomerServicePage from './pages/CustomerServicePage';
+import OwnerBookingDetails from './pages/OwnerBookingDetails';
+import CustomerBookingDetails from './pages/CustomerBookingDetails';
+import OwnerCompletedBooking from './pages/OwnerCompletedBooking';
+import CustomerCompletedBooking from './pages/CustomerCompletedBooking';
+import { UserProvider } from './context/UserContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
-    <Router>
-      <ToastContainerWrapper />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/owner-dashboard" element={<OwnerDash />} />
-        <Route path="/customer-dashboard" element={<CustomerDash />} />
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <ToastContainerWrapper />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/ownerservices" element={<ProtectedRoute role="owner"><OwnerServicePage /></ProtectedRoute>} />
+          <Route path="/customerservices" element={<ProtectedRoute role="customer"><CustomerServicePage /></ProtectedRoute>} />
+          <Route path="/ownerbookdetails" element={<ProtectedRoute role="owner"><OwnerBookingDetails /></ProtectedRoute>} />
+          <Route path="/customerbookdetails" element={<ProtectedRoute role="customer"><CustomerBookingDetails /></ProtectedRoute>} />
+          <Route path="/ownercompleted" element={<ProtectedRoute role="owner"><OwnerCompletedBooking /></ProtectedRoute>} />
+          <Route path="/customercompleted" element={<ProtectedRoute role="customer"><CustomerCompletedBooking /></ProtectedRoute>} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 };
 
